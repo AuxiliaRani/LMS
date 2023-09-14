64175ca0f30e14156d089052fb7e53d80cb3c39f
Auxilia <98889666+AuxiliaRani@users.noreply.github.com>
2023-09-14 07:54:03 -0400

    Update ManageAssignment11.java

--- src/test/java/stepDefinitionsAssignment/ManageAssignment11.java
+++ src/test/java/stepDefinitionsAssignment/ManageAssignment11.java
@@ -9,10 +9,6 @@ import utilities.LoggerLoad;
 
 public class ManageAssignment11 {
 	
-	@When("Admin clicks yes button")
-	public void Admin_clicks_yes_button() {
-		System.out.format("Admin clicks yes button");
-	}
 	@Then("Redirected to assignment page and selected assignment details are deleted from the data table")
 	public void Redirected_to_assignment_page_and_selected_assignment_details_are_deleted_from_the_data_table() {
 		System.out.format("Redirected to assignment page and selected assignment details are deleted ");
@@ -25,10 +21,6 @@ public class ManageAssignment11 {
 	}
 
 	
-	@When("Admin clicks no button")
-	public void Admin_clicks_no_button() {
-		System.out.format("Admin clicks no button");
-	}
 	@Then("Redirected to assignment page and selected assignment details are not deleted from the data table")
 	public void Redirected_to_assignment_page_and_selected_assignment_details_are_not_deleted_from_the_data_table() {
 		System.out.format("Redirected to assignment page and selected assignment details are not deleted");
