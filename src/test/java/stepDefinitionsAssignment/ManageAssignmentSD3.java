31d50f516a29be03d709e2bb013908d92c1335b0
Auxilia <98889666+AuxiliaRani@users.noreply.github.com>
2023-09-14 08:14:36 -0400

    Update ManageAssignmentSD3.java

--- src/test/java/stepDefinitionsAssignment/ManageAssignmentSD3.java
+++ src/test/java/stepDefinitionsAssignment/ManageAssignmentSD3.java
@@ -28,11 +28,6 @@ public class ManageAssignmentSD3 {
 	
 	
 	
-	@When("Admin click +Add new assignment button")
-	public void Admin_click_Add_New_assignment_button() {
-		System.out.format("Admin click +Add new assignment button");
-	}
-	
 	@Then("Admin should see input fields Text (Program name, batch number,Assignment Name, \n"
 			+ "			Assignment Description, grade by, Assignment due date, Assignment File1, Assignment file 2, \n"
 			+ "			Assignment file 3, Assignment file 4, Assignment file 5)")
@@ -43,13 +38,7 @@ public class ManageAssignmentSD3 {
 				+ "				Assignment file3, Assignment file4,Assignment file5");	
 				}
 	
-	
-	
-	@When("Admin click +Add new assignment button")
-	public void Admin_Click_Add_New_Assignment_Button(){
-		System.out.format("Admin click +Add new assignment button");
-	}
-	
+
 	@Then("8 textbox should be  present in Assignment details popup window")
 	public void textbox_should_be_present_in_Assignment_details_popup_window() {
 		System.out.format("Assignment details popup window");
@@ -60,12 +49,7 @@ public class ManageAssignmentSD3 {
 			LoggerLoad.error(e.getMessage());
 		}
 	}
-	
-	
-	@When("Admin click +Add new assignment button")
-	public void Admin_Click_add_New_Assignment_Button(){
-		System.out.format("Admin click +Add new assignment button");
-	}
+
 	@Then("Admin should see  dropdown option for Batch Number")
 	public void Admin_should_see_dropdown_option_for_Batch_Number() {
 		System.out.format("Admin should see  dropdown option for Batch Number");
@@ -77,11 +61,6 @@ public class ManageAssignmentSD3 {
 		}
 	}
 	
-	
-	@When("Admin click +Add new assignment button")
-	public void Admin_click_Add_new_assignment_Button() {
-		System.out.format("Admin click +Add new assignment button");
-	}
 	@Then("Admin should see  dropdown option for Program name")
 	public void Admin_should_see_dropdown_option_for_Program_name() {
 		System.out.format("Admin should see  dropdown option");
@@ -94,10 +73,6 @@ public class ManageAssignmentSD3 {
 	}
 	
 	
-	@When("Admin click +Add new assignment button")
-	public void Admin_Click_Add_new_assignment_Button(){
-		System.out.format("Admin click +Add new assignment button");
-	}
 	@Then("Admin should see  calendar icon for assignment due date")
 	public void Admin_should_see_calendar_icon_for_assignment_due_date() {
 		System.out.format("Admin should see  calendar icon");
@@ -108,21 +83,7 @@ public class ManageAssignmentSD3 {
 			LoggerLoad.error(e.getMessage());
 		}
 	}
-	
-	@When("Admin click +Add new assignment button")
-	public void Admin_Click_Add_New_assignment_button(){
-		System.out.format("Admin click +Add new assignment button");
-	}
-	@Then("Admin should see  save button in the Assignment detail popup window")
-	public void Admin_should_see_save_button_in_the_Assignment_detail_popup_window() {
-		System.out.format("Admin should see  save button");
-	}
-	
-	
-	@When("Admin click +Add new assignment button")
-	public void Admin_Click_Add_new_assignment_button() {
-		System.out.format("Admin click +Add new assignment button");
-	}
+
 	@Then("Admin should see  save button in the Assignment detail popup window")
 	public void Admin_should_see_save_button_In_The_Assignment_detail_popup_window() {
 		System.out.format("Admin should see save button");
@@ -135,10 +96,7 @@ public class ManageAssignmentSD3 {
 	}
 	
 	
-	@When("Admin click +Add new assignment button")
-	public void Admin_Click_Add_New_assignment_Button() {
-		System.out.format("Admin click +Add new assignment button");
-	}
+	
 	@Then("Admin should see  cancel button in the Assignment detail popup window")
 	public void Admin_should_see_cancel_button_in_the_Assignment_detail_popup_window() {
 		System.out.format("Admin should see  cancel button in the Assignment detail popup window");
@@ -151,10 +109,6 @@ public class ManageAssignmentSD3 {
 	}
 	
 	
-	@When("Admin click +Add new assignment button")
-	public void Admin_Click_Add_New_Assignment_button() {
-		System.out.format("Admin click +Add new assignment button");
-	}
 	@Then("Admin should see close button on the Assignment details popup window")
 	public void Admin_should_see_close_button_on_the_Assignment_details_popup_window() {
 		System.out.format("Admin should see close button");
