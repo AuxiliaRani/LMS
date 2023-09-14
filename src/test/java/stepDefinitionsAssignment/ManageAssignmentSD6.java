48b01d9c217779af3e5923d8e896bbcde5465a87
Auxilia <98889666+AuxiliaRani@users.noreply.github.com>
2023-09-14 07:18:17 -0400

    Update ManageAssignmentSD6.java

--- src/test/java/stepDefinitionsAssignment/ManageAssignmentSD6.java
+++ src/test/java/stepDefinitionsAssignment/ManageAssignmentSD6.java
@@ -26,12 +26,12 @@ public class ManageAssignmentSD6 {
 		LoggerLoad.info("Edit assignment detail popup window");
 		
 	}
-	@When("Admin enters all mandatory field values with valid data and clicks save button Program name, batch number,Assignment Name, grade by, Assignment due date")
-	public void Admin_enters_all_mandatory_field_values_with_valid_data_and_clicks_save_button_Program_name_batch_number_Assignment_Name_grade_by_Assignment_due_date() {
+	@When("Admin enters all mandatory field values with valid data and clicks save button Program name, batch number,Assignment Name, grade by, Assignment due date on assignment page")
+	public void Admin_enters_all_mandatory_field_values_with_valid_data_and_clicks_save_button_Program_name_batch_number_Assignment_Name_grade_by_Assignment_due_date_on_assignment_page() {
 		System.out.format("Admin enters all mandatory field values with valid data");
 	}
-	@Then("Admin should see updated assignment details is added in the data table")
-	public void Admin_enters_all_mandatory_field_values_with_valid_data() {
+	@Then("Admin should see updated assignment details is added in the data table on assignment page")
+	public void Admin_enters_all_mandatory_field_values_with_valid_data_on_assignment_page() {
 		System.out.format("Admin should see updated assignment details");
 		
 		List<WebElement> noOfPages = ManageAssignment.updatedassignmentdetails();
@@ -55,13 +55,13 @@ public class ManageAssignmentSD6 {
 	   }   
 	}
 
-	@When("Admin enters all mandatory field values with invalid data and clicks save button Program name, batch number,Assignment Name, grade by, Assignment due date")
-	public void Admin_enters_all_mandatory_field_values_with_invalid_data_and_clicks_save_button_Program_name_batch_number_Assignment_Name_grade_by_Assignment_due_date() {
+	@When("Admin enters all mandatory field values with invalid data and clicks save button Program name, batch number,Assignment Name, grade by, Assignment due date on assignment page")
+	public void Admin_enters_all_mandatory_field_values_with_invalid_data_and_clicks_save_button_Program_name_batch_number_Assignment_Name_grade_by_Assignment_due_date_on_assignment_page() {
 		System.out.format("Admin enters all mandatory field values");
 	}
 	
-	@Then("Error message should appear in alert")
-	public void Error_message_should_appear_in_alert() {
+	@Then("Error message should appear in alert on assignment page")
+	public void Error_message_should_appear_in_alert_on_assignment_page() {
 		System.out.format("Error message should appear in alert");
 		LoggerLoad.info("Error message should appear in alert ");
 		try {
@@ -72,12 +72,12 @@ public class ManageAssignmentSD6 {
 	}
 	
 	
-	@When("Admin enters values in all fields with valid data and clicks save button (Program name, batch number,Assignment Name, Assignment Description, grade by, Assignment due date, Assignment File1, Assignment file 2, Assignment file 3, Assignment file 4, Assignment file 5)")
-	public void Admin_enters_values_in_all_fields_with_valid_data_and_clicks_save_button_Program_name_batch_number_Assignment_Name_Assignment_Description_grade_by_Assignment_due_date_Assignment_File1_Assignment_file_2_Assignment_file_3_Assignment_file_4_Assignment_file_5() {
+	@When("Admin enters values in all fields with valid data and clicks save button (Program name, batch number,Assignment Name, Assignment Description, grade by, Assignment due date, Assignment File1, Assignment file 2, Assignment file 3, Assignment file 4, Assignment file 5) on assignment page")
+	public void Admin_enters_values_in_all_fields_with_valid_data_and_clicks_save_button_Program_name_batch_number_Assignment_Name_Assignment_Description_grade_by_Assignment_due_date_Assignment_File1_Assignment_file_2_Assignment_file_3_Assignment_file_4_Assignment_file_5_on_assignment_page() {
 		System.out.format("Admin enters values in all fields with valid data and clicks save button");
 	}
-	@Then("Admin should see updated assignment details is added in the data tabl")
-	public void Admin_Should_see_updated_assignment_Details_Is_Added_In_the_data_table () {
+	@Then("Admin should see updated assignment details is added in the data tabl on assignment page")
+	public void Admin_Should_see_updated_assignment_Details_Is_Added_In_the_data_table_on_assignment_page () {
 			System.out.format("Admin should see updated assignment details is added in the data tabl");
 			LoggerLoad.info("Admin should see updated assignment details is added in the data tabl ");
 			try {
@@ -88,8 +88,8 @@ public class ManageAssignmentSD6 {
 	}	
 	
 	
-	@When("Admin enters with invalid data in optional fields and clicks save button Assignment File1, Assignment file 2, Assignment file 3, Assignment file 4, Assignment file 5 on assignment page")
-	public void Admin_enters_with_invalid_data_in_optional_fields_and_clicks_save_button_Assignment_File1_Assignment_file_2_Assignment_file_3_Assignment_file_4_Assignment_file_5_on_assignment_page() {
+	@When("Admin enters with invalid data in optional fields and clicks save button Assignment File1, Assignment file 2, Assignment file 3, Assignment file 4, Assignment file 5 on assignment page on assignment page")
+	public void Admin_enters_with_invalid_data_in_optional_fields_and_clicks_save_button_Assignment_File1_Assignment_file_2_Assignment_file_3_Assignment_file_4_Assignment_file_5_on_assignment_page_on_assignment_page() {
 		System.out.format("Admin enters with invalid data in optional fields and clicks save button");
 	}
 	
@@ -111,8 +111,8 @@ public class ManageAssignmentSD6 {
 	}
 
 	
-	@When("Admin enters data missing value in Batch number and clicks save button Program name,Assignment Name, Assignment Description, grade by, Assignment due date, Assignment File1, Assignment file 2, Assignment file 3, Assignment file 4, Assignment file 5")
-	public void Admin_enters_data_missing_value_in_Batch_number_and_clicks_save_button_Program_name_Assignment_Name_Assignment_Description_grade_by_Assignment_due_date_Assignment_File1_Assignment_file_2_Assignment_file3_Assignment_file4_Assignment_file5() {
+	@When("Admin enters data missing value in Batch number and clicks save button Program name,Assignment Name, Assignment Description, grade by, Assignment due date, Assignment File1, Assignment file 2, Assignment file 3, Assignment file 4, Assignment file 5 on assignment page")
+	public void Admin_enters_data_missing_value_in_Batch_number_and_clicks_save_button_Program_name_Assignment_Name_Assignment_Description_grade_by_Assignment_due_date_Assignment_File1_Assignment_file_2_Assignment_file3_Assignment_file4_Assignment_file5_on_assignment_page() {
 		System.out.format("Admin enters data missing value");
 	}
 	@Then("Batch number is missing")
@@ -127,8 +127,8 @@ public class ManageAssignmentSD6 {
 	}
 	
 	
-	@When("Admin enters data missing value in Assignment name and clicks save button Program name, batch number,Assignment Name,grade by,Assignment Due date")
-	public void Admin_enters_data_missing_value_in_Assignment_name_and_clicks_save_button_Program_name_batch_number_Assignment_Name_grade_by_Assignment_Due_date() {
+	@When("Admin enters data missing value in Assignment name and clicks save button Program name, batch number,Assignment Name,grade by,Assignment Due date on assignment page")
+	public void Admin_enters_data_missing_value_in_Assignment_name_and_clicks_save_button_Program_name_batch_number_Assignment_Name_grade_by_Assignment_Due_date_on_assignment_page() {
 		System.out.format("Admin enters data missing value in Assignment name and clicks save button");
 	}
 	@Then("Assignment name is missing")
@@ -143,8 +143,8 @@ public class ManageAssignmentSD6 {
 	}
 	
 	
-	@When("Admin enters data missing value in Assignment due date and clicks save button Program name, batch number,Assignment Name, grade by")
-	public void Admin_enters_data_missing_value_in_Assignment_due_date_and_clicks_save_button_Program_name_batch_number_Assignment_Name_grade_by() {
+	@When("Admin enters data missing value in Assignment due date and clicks save button Program name, batch number,Assignment Name, grade by on assignment page")
+	public void Admin_enters_data_missing_value_in_Assignment_due_date_and_clicks_save_button_Program_name_batch_number_Assignment_Name_grade_by_on_assignment_page() {
 		System.out.format("Admin enters data missing value in Assignment due date and clicks save button");
 	}
 	@Then("Assignment due date is missing")
@@ -154,8 +154,8 @@ public class ManageAssignmentSD6 {
 	}
 	
 	
-	@When("Admin enters data missing value in grade by  and clicks save button Program name, batch number,Assignment Name, Assignment due date")
-	public void Admin_enters_data_missing_value_in_grade_by_and_clicks_save_button_Program_name_batch_number_Assignment_Name_Assignment_due_date() {
+	@When("Admin enters data missing value in grade by  and clicks save button Program name, batch number,Assignment Name, Assignment due date on assignment page")
+	public void Admin_enters_data_missing_value_in_grade_by_and_clicks_save_button_Program_name_batch_number_Assignment_Name_Assignment_due_date_on_assignment_page() {
 		System.out.format("Admin enters data missing value in grade by  and clicks save button");
 	}
 	@Then("Grade by is missing")
@@ -170,12 +170,12 @@ public class ManageAssignmentSD6 {
 	}
 	
 	
-	@When("Admin enters passed date in the due date field and clicks save button Program name, batch number,Assignment Name, grade by, Assignment Due date ")
-	public void Admin_enters_passed_date_in_the_due_date_field_and_clicks_save_button_Program_name_batch_number_Assignment_Name_grade_by_Assignment_Due_date() {
+	@When("Admin enters passed date in the due date field and clicks save button Program name, batch number,Assignment Name, grade by, Assignment Due date on assignment page")
+	public void Admin_enters_passed_date_in_the_due_date_field_and_clicks_save_button_Program_name_batch_number_Assignment_Name_grade_by_Assignment_Due_date_on_assignment_page() {
 		System.out.format("Admin enters passed date in the due date field and clicks save button Program name, batch number,Assignment Name, grade by, Assignment Due date");
 	}
-	@Then("Assignment cannot be updated for the passed date")
-	public void Assignment_cannot_be_updated_for_the_passed_date() {
+	@Then("Assignment cannot be updated for the passed date on assignment page")
+	public void Assignment_cannot_be_updated_for_the_passed_date_on_assignment_page() {
 		System.out.format("Assignment cannot be updated for the passed date");
 		LoggerLoad.info("Assignment cannot be updated for the passed date");
 		try {
@@ -187,8 +187,8 @@ public class ManageAssignmentSD6 {
 	
 	
 	
-	@When("Admin clicks Cancel button without entering values in the fields")
-	public void Admin_clicks_Cancel_button_without_entering_values_in_the_fields() {
+	@When("Admin clicks Cancel button without entering values in the fields on assignment page")
+	public void Admin_clicks_Cancel_button_without_entering_values_in_the_fields_on_assignment_page() {
 		System.out.format("Admin clicks Cancel button without entering values in the fields");
 	}
 	@Then("Admin should land on manage assignment page")
@@ -203,8 +203,8 @@ public class ManageAssignmentSD6 {
 	}
 	
 	
-	@When("Admin clicks Cancel button entering values in the fields")
-	public void Admin_clicks_Cancel_button_entering_values_in_the_fields() {
+	@When("Admin clicks Cancel button entering values in the fields on assignment page")
+	public void Admin_clicks_Cancel_button_entering_values_in_the_fields_on_assignment_page() {
 		System.out.format("Admin clicks Cancel button entering values in the fields");
 	}
 	@Then("Admin should land on manage assignment Page and validate new assignment is not created in the data table for assignment page")
