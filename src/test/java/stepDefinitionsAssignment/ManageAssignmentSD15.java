2224ba80694eddfa33116170f1a7cff101def7a0
Auxilia <98889666+AuxiliaRani@users.noreply.github.com>
2023-09-14 07:44:12 -0400

    Update ManageAssignmentSD15.java

--- src/test/java/stepDefinitionsAssignment/ManageAssignmentSD15.java
+++ src/test/java/stepDefinitionsAssignment/ManageAssignmentSD15.java
@@ -23,13 +23,13 @@ public class ManageAssignmentSD15 {
 			LoggerLoad.error("Assertion failed: " + e.getMessage());
 		}
 	}
-	@When("Admin clicks Student button in the navigation bar")
-	public void Admin_clicks_Student_button_in_the_navigation_bar() {
+	@When("Admin clicks Student button in the navigation bar from assignment page")
+	public void Admin_clicks_Student_button_in_the_navigation_bar_from_assignment_page() {
 		System.out.format("Admin clicks Student button in the navigation bar");
 		ManageAssignment.clickStudentbutton();
 	}
-	@Then("Admin should able to land on student page")
-	public void Admin_should_able_to_land_on_student_page() {
+	@Then("Admin should able to land on student page from assignment page")
+	public void Admin_should_able_to_land_on_student_page_from_assignment_page() {
 		System.out.format("Admin should able to land on student page");
 		try {
 		    
@@ -41,12 +41,12 @@ public class ManageAssignmentSD15 {
 	}
 	
 	
-	@When("Admin clicks Program button in the navigation bar")
-	public void Admin_clicks_Program_button_in_the_navigation_bar() {
+	@When("Admin clicks Program button in the navigation bar from assignment page")
+	public void Admin_clicks_Program_button_in_the_navigation_bar_from_assignment_page() {
 		System.out.format("Admin clicks Program button in the navigation bar");
 	}
-	@Then("Admin should able to land on program page")
-	public void Admin_should_able_to_land_on_program_page() {
+	@Then("Admin should able to land on program page from assignment page")
+	public void Admin_should_able_to_land_on_program_page_from_assignment_page() {
 		System.out.format("Admin should able to land on program page");
 try {
 		    
@@ -57,12 +57,12 @@ try {
 	}
 	}
 	
-	@When("Admin clicks Batch button in the navigation bar")
-	public void Admin_clicks_Batch_button_in_the_navigation_bar() {
+	@When("Admin clicks Batch button in the navigation bar from assignment page")
+	public void Admin_clicks_Batch_button_in_the_navigation_bar_from_assignment_page() {
 		System.out.format("Admin clicks Batch button in the navigation bar");
 	}
-	@Then("Admin should able to land on batch page")
-	public void Admin_should_able_to_land_on_batch_page() {
+	@Then("Admin should able to land on batch page from assignment page")
+	public void Admin_should_able_to_land_on_batch_page_from_assignment_page() {
 		System.out.format("Admin should able to land on batch page");
 try {
 		    
@@ -75,12 +75,12 @@ try {
 
 	
 	
-	@When("Admin clicks User button in the navigation bar")
-	public void Admin_clicks_User_button_in_the_navigation_bar() {
+	@When("Admin clicks User button in the navigation bar from assignment page")
+	public void Admin_clicks_User_button_in_the_navigation_bar_from_assignment_page() {
 		System.out.format("Admin clicks User button in the navigation bar");
 	}
-	@Then("Admin should able to land on user page")
-	public void Admin_should_able_to_land_on_user_page() {
+	@Then("Admin should able to land on user page from assignment page")
+	public void Admin_should_able_to_land_on_user_page_from_assignment_page() {
 	   System.out.format("Admin should able to land on user page");
 	   try {
 		    
@@ -91,12 +91,12 @@ try {
 	}
 	}
 	
-	@When("Admin clicks class button in the navigation bar")
-	public void Admin_clicks_class_button_in_the_navigation_bar() {
+	@When("Admin clicks class button in the navigation bar from assignment page")
+	public void Admin_clicks_class_button_in_the_navigation_bar_from_assignment_page() {
 		System.out.format ("Admin clicks class button in the navigation bar");
 	}
-	@Then("Admin should able to land on class page")
-	public void Admin_should_able_to_land_on_class_page() {
+	@Then("Admin should able to land on class page from assignment page")
+	public void Admin_should_able_to_land_on_class_page_from_assignment_page() {
 		System.out.format("Admin should able to land on class page");
 try {
 		    
@@ -109,12 +109,12 @@ try {
 	}
 	
 
-	@When("Admin clicks Attendance button in the navigation bar")
-	public void Admin_clicks_Attendance_button_in_the_navigation_bar() {
+	@When("Admin clicks Attendance button in the navigation bar from assignment page")
+	public void Admin_clicks_Attendance_button_in_the_navigation_bar_from_assignment_page() {
 		System.out.format ("Admin clicks \" Attendance\" button in the navigation bar");
 	}
-	@Then("Admin should able to land on Attendance page")
-	public void Admin_should_able_to_land_on_Attendance_page() {
+	@Then("Admin should able to land on Attendance page from assignment page")
+	public void Admin_should_able_to_land_on_Attendance_page_from_assignment_page() {
 		System.out.format("Admin should able to land on Attendance page");
 try {
 		    
@@ -125,12 +125,12 @@ try {
 	}
 	}
 
-	@When("Admin clicks Logout button in the navigation bar")
-	public void Admin_clicks_Logout_button_in_the_navigation_bar() {
+	@When("Admin clicks Logout button in the navigation bar from assignment page")
+	public void Admin_clicks_Logout_button_in_the_navigation_bar_from_assignment_page() {
 		System.out.format("Admin clicks Logout buton in the navigation bar");
 	}
-	@Then("Admin should able to land on login page")
-	public void Admin_should_able_to_land_on_login_page() {
+	@Then("Admin should able to land on login page from assignment page")
+	public void Admin_should_able_to_land_on_login_page_from_assignment_page() {
 		System.out.format("Admin should able to land on login page");
 try {
 		    
