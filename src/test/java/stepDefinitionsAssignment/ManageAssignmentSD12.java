7c7a441011f4f4c24a4c554bb0e4200973d5f2dd
Auxilia <98889666+AuxiliaRani@users.noreply.github.com>
2023-09-14 07:59:31 -0400

    Update ManageAssignmentSD12.java

--- src/test/java/stepDefinitionsAssignment/ManageAssignmentSD12.java
+++ src/test/java/stepDefinitionsAssignment/ManageAssignmentSD12.java
@@ -10,10 +10,6 @@ import utilities.LoggerLoad;
 public class ManageAssignmentSD12 {
 
 	
-	@Given("Admin is in manage assignment page")
-	public void Admin_is_in_manage_assignment_page() {
-		System.out.format("Admin is in manage assignment page");
-	}
 	@When("Admin click on assignment name column header to sort")
 	public void Admin_click_on_assignment_name_column_header_to_sort() {
 		System.out.format("Admin click on assignment name column header to sort");
@@ -35,10 +31,7 @@ public class ManageAssignmentSD12 {
 		// TODO Auto-generated method stub
 		return false;
 	}
-	@Given("Admin is in manage assignment page")
-	public void Admin_is_In_manage_assignment_page() {
-		System.out.format("Admin is in manage assignment page");
-	}
+	
 	@When("Admin double click on assignment name column header to sort")
 	public void Admin_double_click_on_assignment_name_column_header_to_sort() {
 		System.out.format("Admin double click on assignment name column header to sort");
