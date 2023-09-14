9c55e9b932c0ad067095122d7774580f39aa8729
Auxilia <98889666+AuxiliaRani@users.noreply.github.com>
2023-09-14 02:31:38 -0400

    Update ManageAssignmentSD.java

--- src/test/java/stepDefinitionsAssignment/ManageAssignmentSD.java
+++ src/test/java/stepDefinitionsAssignment/ManageAssignmentSD.java
@@ -45,15 +45,15 @@ import utilities.LoggerLoad;
 			// TODO Auto-generated method stub
 			return null;
 		}
-		@When("Admin clicks \"assignment\" button on the navigation bar")
-		public void admin_clicks_assignment_button_on_the_navigation_bar() {
+		@When("Admin clicks {string} button on the navigation bar")
+		public void admin_clicks_button_on_the_navigation_bar(String string) {
 			System.out.format("Navigation bar");
 			
 		}
 		
 		
 		@Then("Admin should see URL with \"Manage assignment\"")
-		public void admin_should_url__with_Manage_assignmen(){
+		public void admin_should_url_with_Manage_assignmen(){
 			//System.out.format("Manage assignment");
 			
 			String title = AssignmentPage.verifyPageTitle();
@@ -70,11 +70,7 @@ import utilities.LoggerLoad;
 			// TODO Auto-generated method stub
 			return false;
 		}
-		@When("Admin clicks \"assignment\" button on the navigation bar")
-		public void admin_clicks__assignment_button_on_the_navigation_bar() {
-			System.out.format("click assignmetn");
-			
-		}
+		
 		@Then("Get the response time for navigation from dashboard page to manage assignment page")
 		public void Get_the_response_time_for_navigation_from_dashboard_page_to_manage_assignment_page() {
 			System.out.format("dashboard page");
@@ -88,11 +84,6 @@ import utilities.LoggerLoad;
 		
 		
 		
-		 @When("Admin clicks assignment button on the navigation bar")
-		 public void Admin_clicks_assignment_button_on_the_navigation_bar(){
-			 System.out.format("assignment button");
-			
-		 }
 		 @Then("Admin should see header with Manage assignment")
 		 public void Admin_should_see_header_with_Manage_assignment() {
 			 System .out.format("see Header with manage assignment");
@@ -158,11 +149,6 @@ import utilities.LoggerLoad;
 			
 		}
 		
-		 @When("Admin clicks assignment button on the navigation bar")
-		 public void Admin_clicks_Assignment_button_on_the_navigation_bar() {
-			 System.out.format("assignment button");
-			
-		 }
 		 @Then("Admin should see search bar on the manage assignment page")
 		 public void Admin_should_see_search_bar_on_the_manage_assignment_page() {
 			 System.out.format("clicksearch bar");
@@ -180,11 +166,6 @@ import utilities.LoggerLoad;
 			
 		}
 		
-		 @When("Admin clicks assignment button on the navigation bar")
-		 public void Admin_Clicks_assignment_button_on_the_navigation_bar() {
-			 System.out.format("Click assignmet");
-			 
-		 }
 		 @Then("Admin should see +Add New assignment button on the manage assignment page")
 		 public void Admin_should_see_Add_New_assignment_button_on_the_manage_assignment_page() {
 			 System.out.format("Add assignment");
@@ -202,11 +183,7 @@ import utilities.LoggerLoad;
 			
 		}
 		
-		 @When("Admin clicks assignment button on he navigation bar")
-		 public void Admin_clicks_assignment_button_on_he_navigation_bar() {
-			 System.out.format("click assignment");
-			 
-		 }
+		
 		 @Then("Admin should see data table on the manage assignment page With following column headers "
 		 		+ "add check box symbol Assignment name assignment description Assignment Date Assignment Grade Edit Delete")
 		 public void admin_should_see_data_table_ont_the_manage_assignment_page_With_following_column_headers_check_box_symbol_Assignment_name_Assignment_description_Assignment_Date_Assignment_Grade_Edit_Delete() {
@@ -224,11 +201,7 @@ import utilities.LoggerLoad;
 			
 		}
 		
-		 @When("Admin clicks assignment button on the navigation bar")
-		 public void Admin_clicks_Assignment_Button_on_the_navigation_bar() {
-			 System.out.format("click assignment");
-			 
-		 }
+		
 		@Then("Edit Icon will not be present in data table")
 		public void Edit_Icon_will_not_be_present_in_data_table() {
 			System.out.format("edit Icon");
@@ -246,11 +219,6 @@ import utilities.LoggerLoad;
 			
 		}
 		
-		 @When("Admin clicks assignment button on the navigation bar")
-		 public void Admin_Clicks_Assignment_button_on_the_navigation_bar() {
-			 System.out.format("assignment button on navigation bar");
-			
-		 }
 		 @Then("Delete Icon in each row of data table only when entries are available")
 		 public void Delete_Icon_in_each_row_of_data_table_only_when_entries_are_available() {
 			 System.out.format("delete icon");
@@ -265,11 +233,6 @@ import utilities.LoggerLoad;
 		
 		
 		
-		 @When("Admin clicks assignment button on the navigation bar")
-		 public void Admin_clicks_Assignment_button_On_the_navigation_bar() {
-			 System.out.format("click assignment button");
-			
-		}
 		 @Then("Delete Icon will not be present in data table")
 		 public void Delete_Icon_will_not_be_present_in_data_table() {
 			 System.out.format("delete on data table");
@@ -282,10 +245,7 @@ import utilities.LoggerLoad;
 		 }
 		
 		
-		 @When("Admin clicks assignment button on the navigation bar")
-		 public void Admin_Clicks_Assignment_Button_on_the_navigation_bar() {
-			 System.out.format("assignment button on the navigation bar");
-		 }
+		
 		 @Then("Admin should see sort icon near the column headers except for Edit and Delete")
 		 public void Admin_should_see_sort_icon_near_the_column_headers_except_for_Edit_and_Delete() {
 			 System.out.format("sort icon");
@@ -303,10 +263,7 @@ import utilities.LoggerLoad;
 			
 		}
 		
-		 @When("Admin clicks assignment button on the navigation bar")
-		 public void Admin_Clicks_Assignment_button_on_the_Navigation_bar() {
-			 System.out.format("admin clicks assignment");
-		 }
+		
 		 @Then("Admin should see check box in the all rows of data table when entries available")
 		 public void Admin_should_see_check_box_in_the_all_rows_of_data_table_when_entries_available() {
 			 System.out.format("check box");
@@ -324,10 +281,6 @@ import utilities.LoggerLoad;
 			
 		}
 		
-		 @When("Admin clicks assignment button on the navigation bar")
-		 public void Admin_Clicks_Assignment_Button_On_the_navigation_bar() {
-			 System.out.format("click assignment button on the navigation bar");
-		 }
 		 @Then("Above the footer Admin should see the text as Showing x to y of z entries below the table")
 		 public void Above_the_footer_Admin_should_see_the_text_as_Showing_x_to_y_of_z_entries_below_the_table_x_starting_record_number_on_that_page_y_ending_record_number_on_that_page_z_Total_number_of_records() {
 		 System.out.format("entries below table");
@@ -336,10 +289,7 @@ import utilities.LoggerLoad;
 		
 		
 		
-		 @When("Admin clicks assignment button on the navigation bar")
-		 public void Admin_Clicks_assignment_button_on_the_navigation_Bar() {
-			 System.out.format("click assignment on navigation bar");
-		 }
+		
 		 @Then("Admin should see the page navigation controls under the data table")
 		 public void Admin_should_see_the_page_navigation_controls_under_the_data_table() {
 			 System.out.format("page navigation controls");
@@ -357,10 +307,7 @@ import utilities.LoggerLoad;
 			
 		}
 		
-		 @When("Admin clicks assignment button on the navigation bar")
-		 public void Admin_Clicks_Assignment_Button_On_The_navigation_bar() {
-			 System.out.format("assignment button");
-		 }
+		
 		 @Then("Admin should see the text with total number assignments in the data table In total there are number of assignments")
 		 public void Admin_should_see_the_text_with_total_number_assignments_in_the_data_table_In_total_there_are_number_of_assignments() {
 			 System.out.format("total number of assignment");
