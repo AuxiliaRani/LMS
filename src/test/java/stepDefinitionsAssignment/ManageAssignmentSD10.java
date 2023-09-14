09eac01cfef3169d3dd39690342ac32eca9b61a7
Auxilia <98889666+AuxiliaRani@users.noreply.github.com>
2023-09-14 07:58:48 -0400

    Update ManageAssignmentSD10.java

--- src/test/java/stepDefinitionsAssignment/ManageAssignmentSD10.java
+++ src/test/java/stepDefinitionsAssignment/ManageAssignmentSD10.java
@@ -13,12 +13,9 @@ public class ManageAssignmentSD10 {
 	public void Admin_is_in_delete_alert() {
 		System.out.format("Admin is in delete alert");
 	}
-	@When("Admin clicks yes button")
-	public void Admin_clicks_yes_button() {
-		System.out.format("Admin clicks yes button");
-	}
-	@Then("Redirected to assignment page and selected assignment details are deleted from the data table")
-	public void Redirected_to_assignment_page_and_selected_assignment_details_are_deleted_from_the_data_table() {
+	
+	@Then("Redirected to assignment page and selected assignment details10 are deleted from the data table")
+	public void Redirected_to_assignment_page_and_selected_assignment_details10_are_deleted_from_the_data_table() {
 		System.out.format("Redirected to assignment page and selected assignment details are deleted from the data table");
 		try { 
 			Assert.assertTrue(ManageAssignmentSD10.deleteAlert());}
@@ -33,12 +30,8 @@ public class ManageAssignmentSD10 {
 		return false;
 	}
 	
-	@When("Admin clicks no button")
-	public void Admin_clicks_no_button() {
-		System.out.format("Admin clicks no button");
-	}
-	@Then("Redirected to assignment page and selected assignment details are not deleted from the data table")
-	public void Redirected_to_assignment_page_and_selected_assignment_details_are_not_deleted_from_the_data_table() {
+	@Then("Redirected to assignment page and selected assignment details10 are not deleted from the data table")
+	public void Redirected_to_assignment_page_and_selected_assignment_details10_are_not_deleted_from_the_data_table() {
 		System.out.format("Redirected to assignment page and selected assignment details are not deleted from the data table");
 		System.out.format("Redirected to assignment page and selected assignment details are deleted from the data table");
 		try { 
